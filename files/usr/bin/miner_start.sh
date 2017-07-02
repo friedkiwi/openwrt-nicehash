6d9a4d9ae6bb67dd9e0017ba93021d2a00f8e628
Yvan Janssens <ik@yvanj.me>
2017-07-02 21:50:56 +0100

    Initial commit for the "will it mine?" milestone :-D

---
+++ files/usr/bin/miner_start.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+lsmod | grep nvidia > /dev/null
+
+if [ "$?" -eq "0" ]
+then
+	echo "Nvidia kernel module already loaded - skipping module load."
+else
+	echo "Loading nvidia kernel module..."
+	/usr/sbin/modprobe nvidia
+	if [ "$?" -eq "0" ]
+	then
+		echo "Module load success!"
+	else
+		echo "Failed to load module. Please log into this machine and inspect manually."
+		sleep 5
+		exit 1
+	fi
+fi
+
+
+num_gpus=`lspci | grep VGA | wc -l`
+
+echo "$num_gpus GPUs detected!"
+
+num_gpus=`expr $num_gpus - 1`
+
+
+gpus=""
+
+for i in $(seq 0 $num_gpus)
+do
+	gpus="$gpus $i"
+done
+
+
+/usr/bin/nheqminer  -a 8888 -u `/usr/bin/get_btc_address` -cd $gpus
+
+echo "Mining mining mining!"
